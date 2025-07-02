6c0707053bcc176ba55d05d1ecf0532ccdfe5710
LeoLuosifen <18077921025@163.com>
2025-07-02 21:57:15 +1000

    用户自动登录
    - Created index.ts 用于对登录页面及所需权限进行校验

--- yuoj-frontend/src/App.vue
+++ yuoj-frontend/src/App.vue
@@ -10,9 +10,6 @@
 </style>
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout.vue";
-import { useRouter } from "vue-router";
-import { useStore } from "vuex";
-import ACCESS_ENUM from "@/access/accessEnum";
 import { onMounted } from "vue";
 
 /**
@@ -24,19 +21,5 @@ const doInit = () => {
 
 onMounted(() => {
   doInit();
-});
-
-const router = useRouter();
-const store = useStore();
-
-router.beforeEach((to, from, next) => {
-  // 仅管理员可见
-  if (to.meta?.access === ACCESS_ENUM.ADMIN) {
-    if (store.state.user.loginUser?.userRole !== "admin") {
-      next("/noAuth");
-      return;
-    }
-  }
-  next();
 });
 </script>
