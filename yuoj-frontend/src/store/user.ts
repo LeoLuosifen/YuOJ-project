4ab2078164d289c00803007b709902489ec8ae7e
LeoLuosifen <18077921025@163.com>
2025-07-02 17:36:21 +1000

    前端请求代码生成
    - install axios 和 openapi来使用

--- yuoj-frontend/src/store/user.ts
+++ yuoj-frontend/src/store/user.ts
@@ -1,6 +1,7 @@
 // initial state 存储的状态信息
 import { StoreOptions } from "vuex";
 import ACCESS_ENUM from "@/access/accessEnum";
+import { UserControllerService } from "../../generated";
 
 // mutations（尽量同步） 对变量进行增删改（更新的方法）
 
@@ -11,13 +12,20 @@ export default {
   state: () => ({
     loginUser: {
       userName: "未登录",
-      userRole: ACCESS_ENUM.NOT_LOGIN,
     },
   }),
   actions: {
-    // todo 改为远程请求获取登录信息
-    getLoginUser({ commit, state }, payload) {
-      commit("updateUser", payload);
+    async getLoginUser({ commit, state }, payload) {
+      // 从远程请求获取登录信息
+      const res = await UserControllerService.getLoginUserUsingGet();
+      if (res.code === 0) {
+        commit("updateUser", res.data);
+      } else {
+        commit("updateUser", {
+          ...state.loginUser,
+          userRole: ACCESS_ENUM.NOT_LOGIN,
+        });
+      }
     },
   },
   mutations: {
