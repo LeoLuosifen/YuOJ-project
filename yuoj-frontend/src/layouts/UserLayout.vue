ca46cfe7e9b595e1e5833052e2a5726d25c26fad
LeoLuosifen <18077921025@163.com>
2025-07-02 22:31:02 +1000

    多套布局支持
    - Updated App.vue and routes.ts
    - Created UserLayout.vue, UserLoginView.vue and UserRegisterView.vue 用于展示不同的布局

---
+++ yuoj-frontend/src/layouts/UserLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="userLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header"> 用户布局 </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://leoluosifen.github.io/" target="_blank">5618 by Leo</a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped>
+#userLayout {
+}
+
+#userLayout .header {
+  box-shadow: #b2b2b2 1px 1px 5px;
+}
+
+#userLayout .content {
+  background: linear-gradient(to right, #aaa, #fff);
+  margin-bottom: 16px;
+  padding: 20px;
+}
+
+#userLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: sticky;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+<script setup lang="ts"></script>
